b57d1cc957984c33d77b12f6c0ecab1cb68f0ae7
Vladislav Nurov <v@nurov.ru>
2024-09-05 23:09:15 +0500

    виджет: чат

---
+++ src/widgets/orderChat/model.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+
+export default defineStore<any, any>('orderPreview', {
+  state() {
+    return { order: undefined, chat: undefined }
+  },
+  actions: {
+    fetchOrder() {
+      console.log('call API for get order')
+      this.$patch({
+        order: { id: 1, confirmed: false }
+      })
+    },
+
+    confirmOrder(id: any) {
+      const order = this.order.id === id
+      if (order) {
+        this.order.confirmed = true
+        console.log('call API for confirm')
+      }
+    }
+  }
+})
